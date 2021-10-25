e9a0e18aa62efd9c3774e43182d62cb6e98b5b71
smaster0517 <smaster0517@gmail.com>
2021-10-25 12:05:19 -0300

    moralis web3

---
+++ contracts/Token.sol
@@ -0,0 +1,21 @@
+pragma solidity 0.8.9;
+
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+
+contract Token is ERC721, Ownable {
+
+    struct Pet {
+        uint8 damage; //0-255
+        uint8 magic;
+        uint256 lastMeal;
+        uint256 endurance;
+    }
+
+    mapping( uint256 => Pet) private _tokenDetails;
+
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {
+
+    }
+
+}
